8eff3b5073e751a2e79f6ce49fd1eac29c6ccc83
tonyynot <alramella@gmail.com>
2017-03-18 22:41:45 -0400

    add shuffle-array to increase randomizer

--- tweet.js
+++ tweet.js
@@ -1,61 +1,70 @@
 var Twit = require('twit')
-    fs = require('fs'),
+    fs  = require('fs'),
     path = require('path'),
     Twit = require('twit'),
+    shuffle = require('shuffle-array'),
     dir = require('node-dir'),
     EventEmitter = require('events').EventEmitter,
     filesEE = new EventEmitter(),
     config = require(path.join(__dirname, 'config.js'));
-    images=[];
 
-var tweet = new Twit(config);
 
-var imagesArray = fs.readdirSync('./images/');
-console.log('Opening an image...');
-imagesArray.forEach( function (file) {
-    images.push(file);
-});
+function tweetImage() {
 
-var randImage = images[Math.floor(Math.random()*images.length)];
+    var tweet = new Twit(config);
 
-console.log(randImage);
+    // Define images path
+    var imagesArray = fs.readdirSync('./images/');
 
-function tweetImage(){
+    // Shuffle images
+    collection = imagesArray;
+    shuffle(collection);
 
-  var b64content = fs.readFileSync('./images/' + randImage, { encoding: 'base64' });
+    // Pick random image
+    var randImage = collection[Math.floor(Math.random() * imagesArray.length)];
+    console.log("IMAGE CHOSEN: " + randImage);
 
-  tweet.post('media/upload', { media_data: b64content }, function (err, data, response) {
-    if (err){
-      console.log('ERROR');
-      console.log(err);
-    }
-    else{
+    // Convert content to base64
+    var b64content = fs.readFileSync('./images/' + randImage, {
+        encoding: 'base64'
+    });
 
-      console.log('UPLOADING....');
-
-      tweet.post('statuses/update', {
-        media_ids: new Array(data.media_id_string)
-      },
-        function(err, data, response) {
-          if (err){
-            console.log('aaaaaand its an error.');
+    // Assign b64 content to tweet
+    tweet.post('media/upload', {
+        media_data: b64content
+    },
+    function(err, data, response) {
+        if (err) {
+            console.log('ERROR');
             console.log(err);
-          }
-          else{
-            console.log('_______________________________________________________');
-            console.log('          BLANKBOT HAS TWEETED AN IMAGE!');
-            console.log('          http://twitter.com/blank_waves')
-            console.log('_______________________________________________________');
+        } 
 
-fs.unlink('./images/' + randImage);
-          }
+        else {
+            console.log('UPLOADING....');
+
+            // Post tweet
+            tweet.post('statuses/update', {
+                    media_ids: new Array(data.media_id_string)
+                },
+                function(err, data, response) {
+                    if (err) {
+                        console.log('ERROR');
+                        console.log(err);
+                    } 
+                    else {
+                        console.log('_______________________________________________________');
+                        console.log('          BLANKBOT HAS TWEETED AN IMAGE!');
+                        console.log('          http://twitter.com/blank_waves');
+                        console.log('_______________________________________________________');
+
+                        // Delete image after tweeting
+                        fs.unlinkSync('./images/' + randImage);
+                    }
+                }
+            );
         }
-      );
-    }
-  });
+    });
 }
-
-setInterval(
-  tweetImage(),
-  10000
-);
+setInterval(function() {
+    tweetImage();
+}, 600000 * 6); // Tweets in 1 hour intervals
